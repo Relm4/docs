2448f1eba48dd12124984dc4bb874ed8e1aa0167
Aaron Erhardt <aaron.erhardt@t-online.de>
2022-05-22 15:24:03 +0200

    Fix timestamp formatting
    Signed-off-by: Aaron Erhardt <aaron.erhardt@t-online.de>

--- append_timestamp.sh
+++ append_timestamp.sh
@@ -2,6 +2,6 @@
 
 for var in "$@"
 do
-  TIMESTAMP=`date -u '+\/\/! *This document was built at %A %B %d, %H:%M UTC.'`
+  TIMESTAMP=`date -u '+\/\/! *This document was built at %A %B %d, %H:%M UTC.*'`
   sed "0,/^[[:space:]]*$/ s/^[[:space:]]*$/\/\/!\n$TIMESTAMP\n/" $var -i
 done
