d56549ae4be9dbb2836a1a03114b3cd5233ece56
Aaron Erhardt <aaron.erhardt@t-online.de>
2022-08-04 11:26:58 +0200

    Fix doc workflow
    Signed-off-by: Aaron Erhardt <aaron.erhardt@t-online.de>

--- append_info.sh
+++ append_info.sh
@@ -6,7 +6,7 @@ for var in "$@"
 do
   TIMESTAMP=`date -u '+*This document was built at %A %B %d, %H:%M UTC.*'`
   CARGO_TOML=`echo $var | sed "s/src\/lib.rs/Cargo.toml/"`
-  CRATE=`grep ^name $CARGO_TOML | sed 's/name\s*=\s*"//' | sed 's/"//'`
+  CRATE=`grep ^name $CARGO_TOML | head -1 | sed 's/name\s*=\s*"//' | sed 's/"//'`
 
   if [[ $STABLE == 1 ]]; then
     BOOK_LINK="stable"
@@ -37,8 +37,5 @@ do
   if [[ "//!" == $FIRST_WORD ]]; then
     # Find the first free line after "//!" comments and insert the text there
     sed "0,/^[[:space:]]*$/ s/^[[:space:]]*$/$TEXT\n/" $var -i
-
-    echo " > DEBUG INFO"
-    head $var
   fi
 done
